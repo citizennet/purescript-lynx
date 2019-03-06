3ebd63a8f782484282f5944d9b278a3cd389efe1
Hardy Jones <47793030+joneshf-cn@users.noreply.github.com>
2019-03-06 15:57:29 -0800

    Don't set visibility based on the dropdown
    We were wrong with what this was for.
    It's some dropdown specific behavior.
    When it's visible, the options in the dropdown are shown.
    When it's not visible, the options in the dropdown are not shown.
    
    That's useful to have,
    but We don't want that behavior to change the visibility of the field.
    
    PS-2958 #time 7m

--- src/Page/Form.purs
+++ src/Page/Form.purs
@@ -16,7 +16,7 @@ import Halogen.Component.ChildPath (cp1)
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
-import Lynx.Data.Expr (EvalError(..), Expr, ExprType(..), Key, boolean_, print, reflectType, toBoolean, toString)
+import Lynx.Data.Expr (EvalError(..), Expr, ExprType(..), Key, print, reflectType, toBoolean, toString)
 import Lynx.Data.Form (Field, Input(..), Page, Section, getValue, testPage, userInput)
 import Lynx.Data.Form as Lynx.Data.Form
 import Network.RemoteData (RemoteData(..), fromEither)
@@ -30,7 +30,6 @@ import Ocelot.Block.Toggle (toggle) as Toggle
 import Ocelot.Component.Dropdown as Dropdown
 import Ocelot.Component.Dropdown.Render as Dropdown.Render
 import Ocelot.HTML.Properties (css)
-import Select as Select
 
 type State =
   { form :: RemoteData EvalError { expr :: Page Expr, evaled :: Page ExprType }
@@ -100,13 +99,7 @@ component =
   eval (DropdownQuery key message a) = case message of
     Dropdown.Emit x -> a <$ eval x
     Dropdown.Selected val -> eval (UpdateKey key val a)
-    Dropdown.VisibilityChanged visibility' -> do
-      { form } <- H.get
-      case form of
-        Success { expr } -> do
-          let visibility = boolean_ (visibility' == Select.On)
-          eval (EvalForm (Lynx.Data.Form.setVisibility key visibility expr) a)
-        _ -> pure a
+    Dropdown.VisibilityChanged _ -> pure a
 
   render :: State -> H.ParentHTML Query (ChildQuery m) ChildSlot m
   render { form } =
