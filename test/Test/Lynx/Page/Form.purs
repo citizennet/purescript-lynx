fcad2ea99e1da221aa1c71024a29bc73bb8793a1
Hardy Jones <47793030+joneshf-cn@users.noreply.github.com>
2019-03-08 16:29:23 -0800

    PS-2958 Push all the logic into the do blocks
    It's a bit easier to understand if we're not hopping between data types.
    The fact that it's `StateT _ _ _` is not interesting.
    It's just boilerplate to get things to line up.

--- test/Test/Lynx/Page/Form.purs
+++ test/Test/Lynx/Page/Form.purs
@@ -2,7 +2,9 @@ module Test.Lynx.Page.Form (suite) where
 
 import Prelude
 
-import Control.Monad.State (execStateT)
+import Control.Monad.State (evalStateT)
+import Control.Monad.State.Class (get)
+import Control.Monad.Trans.Class (lift)
 import Data.Map as Data.Map
 import Data.Maybe (Maybe(..))
 import Lynx.Data.Expr (ExprType(..))
@@ -25,38 +27,38 @@ suite = Test.Unit.suite "Test.Lynx.Page.Form" do
   Test.Unit.suite "eval" do
     Test.Unit.suite "DropdownQuery" do
       Test.Unit.suite "Emit" do
-        test "threads queries through appropriately" do
-          newState <- flip execStateT initialState do
-            eval (DropdownQuery "" (Dropdown.Emit $ Initialize unit) unit)
+        test "threads queries through appropriately" $ flip evalStateT initialState do
+          eval (DropdownQuery "" (Dropdown.Emit $ Initialize unit) unit)
 
-          assert "State should have changed" (initialState /= newState)
+          state <- get
+          lift (assert "State should have changed" (initialState /= state))
 
       Test.Unit.suite "Selected" do
-        test "causes the state to be updated" do
+        test "causes the state to be updated" $ flip evalStateT initialState do
           let selected :: ExprType
               selected = Pair { name: String "Cherry", value: String "Cherry" }
 
-          newState <- flip execStateT initialState do
-            eval (Initialize unit)
-            eval (DropdownQuery "food" (Dropdown.Selected selected) unit)
+          eval (Initialize unit)
+          eval (DropdownQuery "food" (Dropdown.Selected selected) unit)
 
+          { values } <- get
           let actual :: Maybe ExprType
-              actual = Data.Map.lookup "food" newState.values
+              actual = Data.Map.lookup "food" values
               expected :: ExprType
               expected = selected
-          equal (Just expected) actual
+          lift (equal (Just expected) actual)
 
       Test.Unit.suite "VisibilityChanged" do
         Test.Unit.suite "On" do
-          test "does nothing" do
-            newState' <- flip execStateT initialState do
-              eval (DropdownQuery "" (Dropdown.VisibilityChanged Select.On) unit)
+          test "does nothing" $ flip evalStateT initialState do
+            eval (DropdownQuery "" (Dropdown.VisibilityChanged Select.On) unit)
 
-            assert "State should not have changed" (initialState == newState')
+            state <- get
+            lift (assert "State should not have changed" (initialState == state))
 
         Test.Unit.suite "On" do
-          test "does nothing" do
-            newState <- flip execStateT initialState do
-              eval (DropdownQuery "" (Dropdown.VisibilityChanged Select.Off) unit)
+          test "does nothing" $ flip evalStateT initialState do
+            eval (DropdownQuery "" (Dropdown.VisibilityChanged Select.Off) unit)
 
-            assert "State should not have changed" (initialState == newState)
+            state <- get
+            lift (assert "State should not have changed" (initialState == state))
